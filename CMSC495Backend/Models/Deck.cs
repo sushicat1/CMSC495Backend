3bfc4da694c3d79ec10149dc920e6d3f4d105f6e
Jeremiah Larsen <decuadros@gmail.com>
2019-07-28 07:10:06 -0700

    Added Deck and Users models.

---
+++ CMSC495Backend/Models/Deck.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CMSC495Backend.Models
+{
+    public class Deck
+    {
+        public int DeckId { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        public string DeckList { get; set; }
+    }
+}
---
+++ CMSC495Backend/Models/Users.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CMSC495Backend.Models
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        public string UserName { get; set; }
+    }
+}
